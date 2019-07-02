b7e875c6005a4bf5c86c559257363a729a3e2a35
taichi ooshima <52149734+k17rs022@users.noreply.github.com>
2019-07-02 13:21:50 +0900

    ログインチェック変更しました

--- pbl/src/sys_check_login.php
+++ pbl/src/sys_check_login.php
@@ -1,19 +1,24 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="Content-TYPE" content="text/html; charset=UTF-8">
+<link rel="stylesheet" TYPE="text/css" href="css/style.css">
+</head>
 <?php
   require_once('db_inc.php'); //データベースが必要なので読み込ませる
-  $u = $_POST['uid'] ;
-  $p = $_POST['upass'];
-  $sql = "SELECT * FROM tbl_user WHERE uid= '{$u}'  AND upass='{$p}'";
+  $u = $_POST['USER_ID'] ;
+  $p = $_POST['USER_PASS'];
+  $sql = "SELECT * FROM T_USER WHERE USER_ID= '{$u}'  AND USER_PASS='{$p}'";
   $rs = mysql_query($sql, $conn);
   if (!$rs) die('エラー: ' . mysql_error());
   $row= mysql_fetch_array($rs);
   if ($row){ //Login succeeded
-    $_SESSION['uid']   = $row['uid'];
-    $_SESSION['uname'] = $row['uname'];
+    $_SESSION['USER_ID']   = $row['USER_ID'];
+    $_SESSION['ACCOUNT_NAME'] = $row['ACOUNT_NAME'];
     $_SESSION['urole'] = $row['urole'];
     header('Location:pb_home.php');
     exit();
   }else{
-    header('Location:sys_login_miss.php');
-    exit();
+  	require_once('sys_login.php');
+    echo'ユーザ名またはパスワードが間違っています。';
   }
 ?>
