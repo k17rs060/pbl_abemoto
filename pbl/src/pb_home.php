cc4d704ebdea2b389a4037eefabc7e574ab442aa
Kenta NOMURA <52149782+k17rs093@users.noreply.github.com>
2019-07-03 20:27:59 +0900

    Update pb_home.php

--- pbl/src/pb_home.php
+++ pbl/src/pb_home.php
@@ -1,7 +1,21 @@
-<h1>  ココすこ！！</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ココすこ！！</title>
+</head>
+<body>
+<h1>ココすこ！！</h1>
+<form action = "hello.php" method = "post">
+ <input type = "submit" value = "店舗登録">　<input type = "submit" value = "ログアウト"><br>
+ </form>
+
+<form action = "hello.php" method = "post">
+ <input type = "text" name = "username">　<input type = "submit" value = "検索"><br>
+ </form>
+
 <?php
 require_once('db_inc.php');
-echo "<h2>店舗一覧</h2>";
 // 一覧データを検索するSQL文
 $sql = "SELECT * FROM T_RSTINFO ORDER BY STORE_NAME, ADDRESS, OP_TIME, CLOSE_TIME, MOVE_TIME, HOLIDAY, HP_URL, EVALUATION";
 // データベースへ問合せのSQL文($sql)を実行する・・・
@@ -14,16 +28,17 @@ if (!$rs) die ('エラー: ' . mysql_error());
 //echo '<tr><th>店舗名</th><th>開始時間～終了時間</th><th>評価</th></tr>';
 
 // 店舗名（STORE_NAME）、営業開始時間（OP_TIME）、営業終了時間（CLOSE_TIME）、評価（EVALUATION）を一覧表示
+echo "<br>";
 $row = mysql_fetch_array($rs);
 while ($row) {
 	echo '<tr>';
-	echo "店舗名" . $row['STORE_NAME'] . "<br>";
-	echo "営業時間" . $row['OP_TIME'];
-	echo " ～ " . $row['CLOSE_TIME'] . "<br>";
-	echo "評価" . $row['EVALUATION'] . "<br>";
-	echo "定休日" . $row['HOLIDAY'];
+	echo "店舗名" . "　" . $row['STORE_NAME'] . "　　　" . "評価" . "　" . $row['EVALUATION'] . "点" . "<br>";
+	echo "<br>";
+	echo "営業時間" . "　" . $row['OP_TIME'] . " 　～　 " . $row['CLOSE_TIME'] . "　　　" . "定休日" . "　" . $row['HOLIDAY']; "<br>";
 	echo '</tr>';
 	$row = mysql_fetch_array($rs); // 次の行へ
 }
 echo '</table>'
 ?>
+</body>
+</html>
