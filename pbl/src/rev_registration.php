8b0945f4bc4117bb68661050b708768f790d7ae4
Kazutaka Oga <52149709+k17rs019@users.noreply.github.com>
2019-07-03 18:27:09 +0900

    口コミ登録機能（画面設計）

---
+++ pbl/src/rev_registration.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UFT-8">
+</head>
+<body>
+<form action="rev_check.php" method="post">
+<h2>ファミレス</h2>
+<h3>口コミ登録</h3>
+<table>
+<tr><td>評価点</td></tr><tr><td>
+ <input type="radio" name="an" value="a"/>1
+ <input type="radio" name="an" value="b"/>2
+ <input type="radio" name="an" value="c" checked/>3
+ <input type="radio" name="an" value="d"/>4
+ <input type="radio" name="an" value="e"/>5
+</td></tr>
+
+<tr><td>コメント</td></tr><tr><td>
+<textarea name="kanso" rows="10" cols="40"> </textarea>
+</td></tr>
+<tr><td>
+<input type="submit" name="a" value="登録"/>
+</td></tr>
+</table>
+</form>
+</body>
+</html>
