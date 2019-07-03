980a6c3a52ddf5b9a5343ca807182a4e4754ebf3
taichi ooshima <52149734+k17rs022@users.noreply.github.com>
2019-07-03 19:42:32 +0900

    店舗削除注意書き

---
+++ pbl/src/res_delete.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="Content-TYPE" content="text/html; charset=UTF-8">
+<link rel="stylesheet" TYPE="text/css" href="css/style.css">
+</head>
+<?php
+require_once('db_inc.php');
+if (isset($_GET['STORE_INFO'])){
+  $STORE_INFO = $_GET['STORE_INFO'];
+  $STORE_NAME = $_GET['STORE_NAME'];
+  echo '<h2>'. $STORE_NAME . 'の店舗情報を削除しますか?</h2>';
+  echo '<a href="res_delete.php?&uuid='. $STORE_INFO . '">はい</a> | <a href=pb_favorg.php>いいえ</a>';
+}else if (isset($_GET['uuid'])){
+   $uid = $_GET['uuid'];
+   $sql = "DELETE FROM t_rstinfo WHERE STORE_INFO='{$uid}'";
+   mysql_query($sql, $conn);
+   header('Location:pb_home.php');
+}else{
+  echo '<h2>削除するユーザIDは与えられていません</h2>';
+  echo '<a href="?do=usr_list">戻る</a>';
+}
+?>
